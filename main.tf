9475e66cead05c839cd1d7841f6b0d915e9c6796
bhuavnp777 <bhuavn.p@amberstudent.com>
2023-04-10 12:44:50 +0530

    testing something 3

--- main.tf
+++ main.tf
@@ -1,4 +1,19 @@
-module "common" {
-  source = "../../../modules/common"
-  env    = "sandbox"
+resource "aws_instance" "example2" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  security_groups = [
+    "sg-0123456789abcdef",
+    "sg-abcdef0123456789",
+  ]
+}
+
+variable "region" {
+  type = string
+  default = "us-west-2"
+}
+
+resource "aws_instance" "example" {
+  ami = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id = var.vpc_id # incorrect variable reference
 }
